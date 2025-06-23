cef7a3fa0a926bd0d29f35cf619c8ec72a767809
derekdv <derek.valencia22@gmail.com>
2025-06-22 22:41:37 -0500

    Update major1.h

--- major1.h
+++ major1.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Function prototype for parity
+// Function for parity check (returns 0 for even, 1 for odd)
 int parity(uint32_t num);
+
+// Function for rotating bits to the right
+uint32_t rotate_right(uint32_t num, uint32_t amount);
 
 #endif
