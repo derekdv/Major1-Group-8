96580ff03bde2be2d7fe0c09e1924391c5fe1a19
derekdv <derek.valencia22@gmail.com>
2025-07-21 22:22:17 -0500

    Update builtins.h

--- Major2_project/builtins.h
+++ Major2_project/builtins.h
@@ -4,4 +4,19 @@
 int is_builtin(const char *cmd);
 void run_builtin(char **args);
 
+// PATH functions
+void init_path();
+void cleanup_path();
+char **get_path_list();
+int is_path_cmd(const char *cmd);
+void run_path(char **args);
+
+// HISTORY functions
+void add_to_history(const char *cmd);
+void show_history();
+void clear_history();
+void execute_history(int index, char *line_buffer);
+int is_history_cmd(const char *cmd);
+void run_history(char **args, char *line_buffer);
+
 #endif
